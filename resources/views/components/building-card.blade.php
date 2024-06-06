302adf2d67d3a14118b45b486d23d4e3d4726ca2
Watcharavit Jiracheeppattana <watcharavit29@gmail.com>
2024-06-06 23:29:04 +0700

    style: edit building-list & building-card for responsive

--- resources/views/components/building-card.blade.php
+++ resources/views/components/building-card.blade.php
@@ -5,14 +5,14 @@
     'building_th',
 ])
 
-<div class="block rounded-lg bg-white shadow-md mt-6 transition-all duration-500 hover:scale-105  hover:shadow-lg">
+<div class="relative flex flex-col bg-clip-border w-[260px] rounded-lg bg-white shadow-md mt-6 transition-all duration-500 hover:scale-105  hover:shadow-lg">
     <a href="{{ $href }}">
         <div class="flex justify-end">
-            <p class="absolute mx-3 my-3 px-2 py-2 bg-green-500 rounded-lg text-sm text-white shadow-md">
+            <p class="absolute mx-3 my-3 px-2 py-2 bg-gradient-to-tr from-green-600 to-green-400 rounded-lg text-sm text-white shadow-md">
                 คงเหลือ 999 ที่นั่ง
             </p>
             <img
-            class="rounded-t-lg w-72 h-48 object-fill"
+            class="rounded-t-lg w-full h-48 object-fill"
             src="https://images.unsplash.com/photo-1554629947-334ff61d85dc?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1024&h=1280&q=80"
             alt="{{ $building_th . 'building_image'}}" 
             />
@@ -25,15 +25,15 @@
                 {{ $building_en }}
             </p>
             <div class="pt-1">
-                <button type="button" class="group inline-block rounded px-12 py-1 bg-green-500 border-2  border-green-500 transition duration-500 ease-in-out hover:scale-105">
+                <button type="button" class="group inline-block rounded py-1 bg-gradient-to-tr from-green-600 to-green-400 shadow-md shadow-green-500/20 hover:shadow-lg hover:shadow-green-500/40 transition duration-500 ease-in-out hover:scale-105 px-14">
                     <p class="text-white">
                         เลือก
                     </p>
                 </button>
-                <button type="button" class="group pl-4 inline-block transition duration-500 ease-in-out hover:scale-110">
+                <button type="button" class="group pl-3 inline-block transition duration-500 ease-in-out hover:scale-110">
                     <svg xmlns="http://www.w3.org/2000/svg"  class="inline-block fill-black group-hover:fill-yellow-500" id="Outline" viewBox="0 0 24 24" width="24" height="24"><path d="M22.853,1.148a3.626,3.626,0,0,0-5.124,0L1.465,17.412A4.968,4.968,0,0,0,0,20.947V23a1,1,0,0,0,1,1H3.053a4.966,4.966,0,0,0,3.535-1.464L22.853,6.271A3.626,3.626,0,0,0,22.853,1.148ZM5.174,21.122A3.022,3.022,0,0,1,3.053,22H2V20.947a2.98,2.98,0,0,1,.879-2.121L15.222,6.483l2.3,2.3ZM21.438,4.857,18.932,7.364l-2.3-2.295,2.507-2.507a1.623,1.623,0,1,1,2.295,2.3Z"/></svg>
                 </button>
-                <button type="button" class="group pl-3 inline-block transition duration-500 ease-in-out hover:scale-110 ">
+                <button type="button" class="group pl-2 inline-block transition duration-500 ease-in-out hover:scale-110 ">
                     <svg xmlns="http://www.w3.org/2000/svg" class="inline-block fill-black group-hover:fill-red-400" id="Outline" viewBox="0 0 24 24" width="24" height="24"><path d="M21,4H17.9A5.009,5.009,0,0,0,13,0H11A5.009,5.009,0,0,0,6.1,4H3A1,1,0,0,0,3,6H4V19a5.006,5.006,0,0,0,5,5h6a5.006,5.006,0,0,0,5-5V6h1a1,1,0,0,0,0-2ZM11,2h2a3.006,3.006,0,0,1,2.829,2H8.171A3.006,3.006,0,0,1,11,2Zm7,17a3,3,0,0,1-3,3H9a3,3,0,0,1-3-3V6H18Z"/><path d="M10,18a1,1,0,0,0,1-1V11a1,1,0,0,0-2,0v6A1,1,0,0,0,10,18Z"/><path d="M14,18a1,1,0,0,0,1-1V11a1,1,0,0,0-2,0v6A1,1,0,0,0,14,18Z"/></svg>
                 </button>
             </div>
--- resources/views/pages/building-list.blade.php
+++ resources/views/pages/building-list.blade.php
@@ -1,11 +1,11 @@
 @extends('layouts/main')
 
 @section('content')
-<div class="flex flex-col divide-y z-20 container mt-24 ml-8 mb-24 lg:ml-80">
+<div class="flex flex-col divide-y z-20 container mt-24 ml-8 mb-24 lg:ml-80 2xl:pl-24 ">
     <p class="font-semibold text-2xl">
         อาคารสอบทั้งหมด
     </p>
-    <div class="grid grid-cols-4 gap-4 mt-2 mr-4">
+    <div class="grid 2xl:grid-cols-5 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-3 sm:grid-cols-2 gap-4 mt-2">
         @foreach ($buildings as $building)
             <x-building-card
                 :building_image="file_exists('storage/' . $building->building_image) ? asset('storage/' . $building->building_image) : $building->building_image"
