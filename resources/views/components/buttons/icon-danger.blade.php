0262409e57afd6e11574c838d041461c8623de64
Watcharavit Jiracheeppattana <watcharavit29@gmail.com>
2024-06-07 11:19:04 +0700

    refactor: button.icon components

--- resources/views/components/buttons/icon-danger.blade.php
+++ resources/views/components/buttons/icon-danger.blade.php
@@ -1,7 +1,5 @@
-<a href="{{ $route }}" class="">
-   <button {{ $attributes->merge(['class' => 'group rounded-full transition-all duration-500 hover:scale-110 bg-gradient-to-tr hover:from-red-600 hover:to-red-400 hover:border-white hover:shadow-lg']) }}>
-        <div {{ $attributes->merge(['class' => 'fill-black transition-all duration-300 group-hover:scale-110 group-hover:fill-white'])}}>
-            {{ $slot }}
-        </div>
-    </button>
-</a>
+<button {{ $attributes->merge(['class' => 'group rounded-full transition-all duration-500 hover:scale-110 bg-gradient-to-tr hover:from-red-600 hover:to-red-400 hover:border-white hover:shadow-lg']) }}>
+    <div {{ $attributes->merge(['class' => 'fill-black transition-all duration-300 group-hover:scale-110 group-hover:fill-white'])}}>
+        {{ $slot }}
+    </div>
+</button>
