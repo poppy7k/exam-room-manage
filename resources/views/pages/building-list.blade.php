cf7f5bbca6dddbeff1c69dca574af490ad0a5bc5
Watcharavit Jiracheeppattana <watcharavit29@gmail.com>
2024-06-07 10:52:26 +0700

    refactor: delete building button onclick

--- resources/views/pages/building-list.blade.php
+++ resources/views/pages/building-list.blade.php
@@ -6,7 +6,7 @@
         <p class="font-semibold text-2xl justify-start">
             อาคารสอบทั้งหมด
         </p>
-        <x-buttons.icon-primary type="submit" class="px-1 py-1 z-40">
+        <x-buttons.icon-primary onclick="{{ route('buildings.addbuilding') }}" type="submit" class="px-1 py-1 z-40">
             <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Capa_1" x="0px" y="0px" viewBox="0 0 512 512" style="enable-background:new 0 0 512 512;" xml:space="preserve" width="16" height="16"><g><path d="M480,224H288V32c0-17.673-14.327-32-32-32s-32,14.327-32,32v192H32c-17.673,0-32,14.327-32,32s14.327,32,32,32h192v192   c0,17.673,14.327,32,32,32s32-14.327,32-32V288h192c17.673,0,32-14.327,32-32S497.673,224,480,224z"/></g>/</svg>
             <x-tooltip title="เพิ่มอาคาร" class="group-hover:-translate-x-7"></x-tooltip>
         </x-buttons.icon-primary>
