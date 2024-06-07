4c88460bbf91ef3d1f093d88198a1a359d0d98b6
Watcharavit Jiracheeppattana <watcharavit29@gmail.com>
2024-06-07 11:10:09 +0700

    feat: route button AND create building button route

--- resources/views/components/building-card.blade.php
+++ resources/views/components/building-card.blade.php
@@ -32,16 +32,16 @@
                 </p>
             </div>
             <div class="flex justify-between pt-1">
-                <button type="button" class="group inline-block rounded bg-gradient-to-tr from-green-600 to-green-400 shadow-md shadow-green-500/20 hover:shadow-lg hover:shadow-green-500/40 transition duration-500 ease-in-out hover:scale-105 px-12">
+                <button type="button" class="group inline-block py-1 rounded bg-gradient-to-tr from-green-600 to-green-400 shadow-md shadow-green-500/20 hover:shadow-lg hover:shadow-green-500/40 transition duration-500 ease-in-out hover:scale-105 px-12">
                     <p class="text-white">
                         เลือก
                     </p>
                 </button>
-                <x-buttons.icon-info type="submit" class="px-1 py-1">
+                <x-buttons.icon-info type="submit" route="#" class="px-1 py-1">
                     <svg xmlns="http://www.w3.org/2000/svg"  class="" id="Outline" viewBox="0 0 24 24" width="20" height="20"><path d="M22.853,1.148a3.626,3.626,0,0,0-5.124,0L1.465,17.412A4.968,4.968,0,0,0,0,20.947V23a1,1,0,0,0,1,1H3.053a4.966,4.966,0,0,0,3.535-1.464L22.853,6.271A3.626,3.626,0,0,0,22.853,1.148ZM5.174,21.122A3.022,3.022,0,0,1,3.053,22H2V20.947a2.98,2.98,0,0,1,.879-2.121L15.222,6.483l2.3,2.3ZM21.438,4.857,18.932,7.364l-2.3-2.295,2.507-2.507a1.623,1.623,0,1,1,2.295,2.3Z"/></svg>
                     <x-tooltip title="แก้ไขข้อมูล" class="group-hover:-translate-x-6"></x-tooltip>
                 </x-buttons.icon-info>
-                <x-buttons.icon-danger type="delete" onclick="deleteBuilding('{{ $building_id }}')" class="px-1 py-1">
+                <x-buttons.icon-danger type="delete" route="#" onclick="deleteBuilding('{{ $building_id }}')" class="px-1 py-1">
                     <svg xmlns="http://www.w3.org/2000/svg" class="" id="Outline" viewBox="0 0 24 24" width="20" height="20"><path d="M21,4H17.9A5.009,5.009,0,0,0,13,0H11A5.009,5.009,0,0,0,6.1,4H3A1,1,0,0,0,3,6H4V19a5.006,5.006,0,0,0,5,5h6a5.006,5.006,0,0,0,5-5V6h1a1,1,0,0,0,0-2ZM11,2h2a3.006,3.006,0,0,1,2.829,2H8.171A3.006,3.006,0,0,1,11,2Zm7,17a3,3,0,0,1-3,3H9a3,3,0,0,1-3-3V6H18Z"/><path d="M10,18a1,1,0,0,0,1-1V11a1,1,0,0,0-2,0v6A1,1,0,0,0,10,18Z"/><path d="M14,18a1,1,0,0,0,1-1V11a1,1,0,0,0-2,0v6A1,1,0,0,0,14,18Z"/></svg>
                     <x-tooltip title="ลบอาคาร" class="group-hover:-translate-x-5"></x-tooltip>
                 </x-buttons.icon-danger>
