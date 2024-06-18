129bc490d51d8f8f5072834f7a450bac703bdd16
Watcharavit Jiracheeppattana <watcharavit29@gmail.com>
2024-06-18 11:28:08 +0700

    feat: exam-create time validation

--- resources/views/pages/exam-manage/exam-create.blade.php
+++ resources/views/pages/exam-manage/exam-create.blade.php
@@ -10,15 +10,15 @@
         <div class="mb-4">
             <label for="exam_name" class="block font-semibold">ชื่อการสอบ</label>
             <input type="text" list="exam_name_list" id="exam_name" name="exam_name" placeholder="กรอกชื่อการสอบ" required class="w-full my-2 px-3 py-2 rounded ring-1 shadow-sm ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-green-600 transition-all duration-300 outline-none">
-            <datalist id="exam_name_list">
+        </div>
+        <div class="mb-4">
+            <label for="department_name" class="block font-semibold">ชื่อฝ่ายงาน</label>
+            <input list="department_list" type="text" id="department_name" name="department_name" placeholder="กรอกชื่อฝ่ายงาน" required class="w-full my-2 px-3 py-2 rounded ring-1 shadow-sm ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-green-600 transition-all duration-300 outline-none">
+            <datalist id="department_list">
                 <option value="Option 1"></option>
                 <option value="Option 2"></option>
                 <option value="Option 3"></option>
              </datalist>
-        </div>
-        <div class="mb-4">
-            <label for="department_name" class="block font-semibold">ชื่อฝ่ายงาน</label>
-            <input type="text" id="department_name" name="department_name" placeholder="กรอกชื่อฝ่ายงาน" required class="w-full my-2 px-3 py-2 rounded ring-1 shadow-sm ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-green-600 transition-all duration-300 outline-none">
         </div>
         <div class="flex mb-4 justify-between">
             <div class="">
@@ -27,11 +27,20 @@
             </div>
             <div class="">
                 <label for="exam_start_time" class="block font-semibold">เวลาเริ่มการสอบ</label>
-                <input type="time" id="exam_start_time" name="exam_start_time" placeholder="เวลาเริ่มการสอบ" required class="w-full my-2 px-3 py-2 rounded ring-1 shadow-sm ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-green-600 transition-all duration-300 outline-none">
+                <input list="time_list" type="text" id="exam_start_time" name="exam_start_time" placeholder="เวลาเริ่มการสอบ" required class="w-full my-2 px-3 py-2 rounded ring-1 shadow-sm ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-green-600 transition-all duration-300 outline-none">
+                <span id="exam_start_time_error" class="error-message" style="color: red; display: none;">* กรุณากรอกเวลา 00:00 และหน่วยนาทีเป็น 00, 30 เท่านั้น!</span>
             </div>
             <div class="">
                 <label for="exam_end_time" class="block font-semibold">เวลาสิ้นสุดการสอบ</label>
-                <input type="time" id="exam_end_time" name="exam_end_time" placeholder="เวลาสิ้นสุดการสอบ" required class="w-full my-2 px-3 py-2 rounded ring-1 shadow-sm ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-green-600 transition-all duration-300 outline-none">
+                <input list="time_list" type="text" id="exam_end_time" name="exam_end_time" placeholder="เวลาสิ้นสุดการสอบ" required class="w-full my-2 px-3 py-2 rounded ring-1 shadow-sm ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-green-600 transition-all duration-300 outline-none">
+                <span id="exam_end_time_error" class="error-message" style="color: red; display: none;">* กรุณากรอกเวลา 00:00 และหน่วยนาทีเป็น 00, 30 เท่านั้น!</span>
+                <datalist id="time_list">
+                    @for ($hour = 0; $hour < 24; $hour++)
+                        @for ($minute = 0; $minute < 60; $minute += 30)
+                            <option value="{{ sprintf('%02d', $hour) }}:{{ sprintf('%02d', $minute) }}"></option>
+                        @endfor
+                    @endfor
+                </datalist>
             </div>
         </div>
         <div class="mb-4">
@@ -51,8 +60,31 @@
 
 <script>
     function validateForm() {
-        // Add your form validation logic here if needed
-        return true;
+        var isValid = true;
+        var examStartTime = document.getElementById('exam_start_time');
+        var examStartTimeError = document.getElementById('exam_start_time_error');
+
+        var examEndTime = document.getElementById('exam_end_time');
+        var examEndTimeError = document.getElementById('exam_end_time_error');
+        var regex = /^(?:2[0-3]|[01]?[0-9]):(00|30)$/; // เช็ครูปแบบเวลา HH:MM
+        if (!regex.test(examStartTime.value)) {
+            examStartTime.value = "";
+            examStartTime.focus();
+            isValid = false;
+            examStartTimeError.style.display = 'block';
+        } else {
+            examStartTimeError.style.display = 'none';
+        }
+        if (!regex.test(examEndTime.value)) {
+            examEndTime.value = "";
+            examEndTime.focus();
+            isValid = false;
+            examEndTimeError.style.display = 'block';
+        } else {
+            examEndTimeError.style.display = 'none';
+        }
+        
+        return isValid;
     }
 </script>
 @endsection
